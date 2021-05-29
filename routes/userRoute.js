75fd6147cb3640d687219ffe57a6024f1c7f6e66
unknown <ashutoshthakur_14@yahoo.com>
2021-05-29 18:42:11 +0530

    Get profile details added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/user.controller')
+const { isAuth } = require('../middleware/utils')
+const { register, login, getUserdetails } = require('../controllers/user.controller')
 
 router.post('/register', register)
 
 router.post('/login', login)
+
+router.get('/', isAuth, getUserdetails)
 
 module.exports = router;
